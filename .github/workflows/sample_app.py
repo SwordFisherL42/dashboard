a2805e98ee5802ea1be6a1cc253a95312f40a667
Fisher <SwordFisherL42@gmail.com>
2024-07-08 21:03:21 -0400

    Persistant Steps workflow Test

--- .github/workflows/sample_app.py
+++ .github/workflows/sample_app.py
@@ -8,7 +8,17 @@ if __name__ == "__main__":
     args = parser.parse_args(sys.argv[1:])
     os.environ['github_link'] = 'http://github.com/runs/1234'
     os.environ['environment'] = 'development'
-    
+    # Define the environment variable
+    env_var = "github_link=http://github.com/runs/1234"
+    env_var_2 = "development"
+    # Get the path to the GITHUB_ENV file
+    github_env = os.getenv('GITHUB_ENV')
+
+    # Write the environment variable to the GITHUB_ENV file
+    if github_env:
+        with open(github_env, 'a') as env_file:
+            env_file.write(env_var + '\n')
+            env_file.write(env_var_2 + '\n')
     if args.action == "pass":
         print("Sample App Pass")
         os.environ['message_content'] = 'Orchestor Passed Successfully'
