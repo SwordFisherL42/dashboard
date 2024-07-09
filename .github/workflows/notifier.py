025d6b702a0711309abc192b8d35b555e26e8d00
Fisher <SwordFisherL42@gmail.com>
2024-07-08 21:52:28 -0400

    Persistant Steps workflow Test

--- .github/workflows/notifier.py
+++ .github/workflows/notifier.py
@@ -5,13 +5,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("status")
     parser.add_argument("info")
-    parser.add_argument("link")
     args = parser.parse_args(sys.argv[1:])
+    info_data = args.info.split(",")
+    github_link = None
+    environment = None
+    commit_sha = None
+    for data in info_data:
+        if "github_link:" in data:
+            github_link = data.split("github_link:").strip(" ,")
+        elif "environment:" in data:
+            environment = data.split("environment:").strip(" ,")
+        elif "commit:" in data:
+            commit_sha = data.split("commit:").strip(" ,")
     if args.status == "0":
-        print("Notify of Pass")
+        print(f"Sucessfully Deployed on Commit {commit_sha} for environment {environment}\nGithub Link: {github_link}")
         # Success Message
     else:
-        print("Notify of Failure")
+        print(f"Failed to deploy on Commit {commit_sha} for environment {environment}\nGithub Link: {github_link}")
         # Failure Message
     print(args.info)
-    print(args.link)
--- .github/workflows/sample_app.py
+++ .github/workflows/sample_app.py
@@ -8,10 +8,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("action", choices=["pass", "fail"])
     args = parser.parse_args(sys.argv[1:])
+    logging.info("environment: %s" % "development")
+    logging.info("commit: %s" % "5678abcd")
     if args.action == "pass":
         print("Sample App Pass")
         logging.info("Github Link: %s" % "http://github.com/1234")
-        logging.info("environment: %s" % "development")
     else:
         print("Sample App FAIL")
         try:
