c5d2e15bb8e27bcc5a6b58bedaf944105ff46066
Fisher <SwordFisherL42@gmail.com>
2024-07-08 20:58:15 -0400

    Persistant Steps workflow Test

--- .github/workflows/notifier.py
+++ .github/workflows/notifier.py
@@ -5,9 +5,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("status")
     parser.add_argument("info")
+    parser.add_argument("link")
     args = parser.parse_args(sys.argv[1:])
     if args.status == "0":
         print("Notify of Pass")
     else:
         print("Notify of Failure")
     print(args.info)
+    print(args.link)
--- .github/workflows/sample_app.py
+++ .github/workflows/sample_app.py
@@ -6,9 +6,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("action", choices=["pass", "fail"])
     args = parser.parse_args(sys.argv[1:])
+    os.environ['github_link'] = 'http://github.com/runs/1234'
+    os.environ['environment'] = 'development'
+    
     if args.action == "pass":
         print("Sample App Pass")
+        os.environ['message_content'] = 'Orchestor Passed Successfully'
     else:
+        os.environ['message_content'] = 'Orchestor Failed'
         with open("./build_file.txt", "w") as fh:
             fh.write("Sample App Log")
         print("Sample App FAIL")
