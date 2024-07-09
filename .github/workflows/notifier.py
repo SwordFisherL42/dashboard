0076348f68f59a44fd25645e7c4e8e4f9527c6bd
Fisher <SwordFisherL42@gmail.com>
2024-07-08 20:47:26 -0400

    Persistant Steps workflow Test

--- .github/workflows/notifier.py
+++ .github/workflows/notifier.py
@@ -3,10 +3,10 @@ import sys
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("result")
+    parser.add_argument("status")
     parser.add_argument("info")
     args = parser.parse_args(sys.argv[1:])
-    if not args.action:
+    if args.status == "0":
         print("Notify of Pass")
     else:
         print("Notify of Failure")
