6eef759f2721ee7d748acd51cb063ebedbab4e2a
Fisher <SwordFisherL42@gmail.com>
2024-07-10 12:42:52 -0400

    Persistant Steps workflow Test

--- .github/workflows/notifier.py
+++ .github/workflows/notifier.py
@@ -6,23 +6,25 @@ if __name__ == "__main__":
     parser.add_argument("status")
     parser.add_argument("info")
     args = parser.parse_args(sys.argv[1:])
-    info_data = args.info.split(",")
+    # info_data = args.info.split(",")
     github_link = None
     environment = None
     commit_sha = None
     error = None
-    for data in info_data:
-        if "Github Link:" in data:
-            github_link = data.split("Github Link:")[1].strip(" ,")
-        elif "environment:" in data:
-            environment = data.split("environment:")[1].strip(" ,")
-        elif "commit:" in data:
-            commit_sha = data.split("commit:")[1].strip(" ,")
-        elif "error:" in data:
-            error = data.split("error:")[1].strip(" ,")
+    # for data in info_data:
+    #     if "Github Link:" in data:
+    #         github_link = data.split("Github Link:")[1].strip(" ,")
+    #     elif "environment:" in data:
+    #         environment = data.split("environment:")[1].strip(" ,")
+    #     elif "commit:" in data:
+    #         commit_sha = data.split("commit:")[1].strip(" ,")
+    #     elif "error:" in data:
+    #         error = data.split("error:")[1].strip(" ,")
     if args.status == "0":
-        print(f"Sucessfully Deployed on Commit {commit_sha} for environment {environment}\nGithub Link: {github_link}")
+        print(f"Success message: {args.status} {args.info}")
+        # print(f"Sucessfully Deployed on Commit {commit_sha} for environment {environment}\nGithub Link: {github_link}")
         # Success Message
     else:
-        print(f"Failed to deploy on Commit {commit_sha} for environment {environment}\nGithub Link: {github_link}\nError: {error}")
+        print(f"Fail message: {args.status} {args.info}")
+        # print(f"Failed to deploy on Commit {commit_sha} for environment {environment}\nGithub Link: {github_link}\nError: {error}")
         # Failure Message
--- .github/workflows/sample_app.py
+++ .github/workflows/sample_app.py
@@ -3,20 +3,48 @@ import sys
 import os
 import logging
 
+def write_to_github_output(name: str, value: str):
+    # is_action = os.getenv('GITHUB_ACTION')
+    # output_path = os.getenv('GITHUB_OUTPUT')
+    # if is_action and output_path:
+    #     with open(output_path, 'a') as fh:
+    #         fh.write(f"{name}={value}\n")
+    # else:
+    #     logging.info("Skipping write to GithHub outputs. (Not running via GitHub Actions)")
+    if os.getenv('GITHUB_ACTION'):
+        try:
+            with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+                fh.write(f"{name}={value}\n")
+        except KeyError as key_error:
+            logging.exception(f"Unable to to write to GitHub actions")
+            raise key_error
+        except IOError as io_error:
+            logging.exception(f"Unable to to write to GitHub actions")
+            raise io_error
+    else:
+        logging.info("Skipping write to GithHub outputs. (Not running via GitHub Actions)")
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s,", stream=sys.stdout)
     parser = argparse.ArgumentParser()
     parser.add_argument("action", choices=["pass", "fail"])
     args = parser.parse_args(sys.argv[1:])
-    logging.info("environment: %s" % "development")
-    logging.info("commit: %s" % "5678abcd")
+    # write_to_github_output("info", "http://github.com/1234")
+    # write_to_github_output("info", "http://github.com/1234")
+    # write_to_github_output("info", "http://github.com/1234")
     if args.action == "pass":
         print("Sample App Pass")
-        logging.info("Github Link: %s" % "http://github.com/1234")
+        write_to_github_output("status", "0")
+        write_to_github_output("info", "success in deployment")
     else:
         print("Sample App FAIL")
         try:
             x = 1/0
         except Exception as e:
             logging.error("error: %s" % str(e))
+            write_to_github_output("status", "error")
+            write_to_github_output("info", "error failed to deploy")
+            write_to_github_output("error", str(e))
+            write_to_github_output("link", "http://github.com/1234")
             raise e
